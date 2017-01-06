8cae238eca8340871a138e2ffcca6e720b288542
Shashikant <shashikant.jagtap@icloud.com>
2017-01-06 07:05:11 +0000

    Update homescript.rb

--- homescript.rb
+++ homescript.rb
@@ -7,6 +7,7 @@ class Homescript < Formula
   homepage "https://github.com/Shashikant86/homebrew-taps"
   url "https://github.com/Shashikant86/HomeScript/archive/v2.0.0.tar.gz"
   version "v2.0.0"
+  sha256 "90cd809cb27c61632897d7dd721c139ac985b16234885da0194ede326154b7ec"
  
 
 
