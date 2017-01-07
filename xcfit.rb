237e3c478833f483ba45e4c34d6e14293e7bfc81
Shashikant86 <shashikant.jagtap@icloud.com>
2017-01-07 10:32:25 +0000

    Adding homeBrew Support for XCFit

--- xcfit.rb
+++ xcfit.rb
@@ -6,7 +6,7 @@ class Xcfit < Formula
   desc "XCFit: Full Stack BDD framework for iOS app"
   homepage "https://shashikant86.github.io/XCFit/"
   url "https://github.com/Shashikant86/XCFit/archive/v2.1.tar.gz"
-  sha256 "b2f388c49f1757b5db7f3aea89bd4d857a3fdd1c1489bdcb5479c6fea2708615"
+  sha256 "731f17a1b245bff99a1134672c0d4ffc3cf0fa447f1e45c4fd2562e0681037ea"
 
   def install
     bin.install 'homebrew/xcfit'
