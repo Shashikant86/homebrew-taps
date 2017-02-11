90ae40c13cd8b65b4785be554e9112852f009f4d
Shashikant <shashikant.jagtap@icloud.com>
2017-02-11 09:18:00 +0000

    Update xcfit.rb

--- xcfit.rb
+++ xcfit.rb
@@ -1,7 +1,7 @@
 class Xcfit < Formula
   desc "XCFit: Full Stack BDD framework for iOS app"
   homepage "https://shashikant86.github.io/XCFit/"
-  url "https://github.com/Shashikant86/XCFit/archive/3.1.0.tar.gz"
+  url "https://github.com/Shashikant86/XCFit/archive/3.1.1.tar.gz"
   sha256 "95deebc7e242ae83be2f8d187bb26c9cbf45a90604b0ddaec5317c3e226cb446"
 
   def install
